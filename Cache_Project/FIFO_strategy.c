a865029c3de7be11d31b7aa8ccedfa37edce1184
Tom Dall'Agnol <dallagnol.tom@etu.unice.fr>
2015-06-08 14:23:38 +0200

    Fin du FIFO_strategy.c . Tout fonctionne avec les tests

--- Cache_Project/FIFO_strategy.c
+++ Cache_Project/FIFO_strategy.c
@@ -2,37 +2,40 @@
 
 #include "strategy.h"
 #include "low_cache.h"
-#include "time.h"
 #include "cache_list.h"
 
 /*
 Liste pointeur sur blocs valides
 Des que bloc non valide devient valide on le met en queue
 affectation a un autre bloc : on le met en queue
-
 */
 
-
+/*
+ * On doit créer la liste qui servira de queue pour le FIFO
+ */
 void *Strategy_Create(struct Cache *pcache) 
 {
-	//initialisation liste
-	struct Cache_List *cache_list = Cache_List_Create();
-	pcache->pstrategy = cache_list;
-
-	return NULL;	
+	return Cache_List_Create();	
 }
 
+/* On détruit la liste pour libérer de la mémoire*/
 void Strategy_Close(struct Cache *pcache)
 {
-	Cache_List_Delete(pcache->pstrategy);
+	Cache_List_Delete((struct Cache_List *) pcache->pstrategy);
 }
 
+/* On vide la liste de tous les éléments qui la compose*/
 void Strategy_Invalidate(struct Cache *pcache)
 {
-
-    
+	Cache_List_Clear((struct Cache_List *)pcache->pstrategy);    
 }
 
+/*
+ * La stratégie du FIFO : 
+ * 		chercher un bloc libre. 
+ *		si pas de bloc libre : enlever le bloc le plus ancien
+ *  en terme de création et renvoyer l'adresse de ce bloc nouvellement vide 
+ */
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
 {
 	struct Cache_Block_Header *pbh;
@@ -47,19 +50,20 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache)
 	pbh = Cache_List_Remove_First(((struct Cache_List *)(pcache->pstrategy)));
 	/*rajoute le bloc à la fin de la queue*/
 	Cache_List_Append(((struct Cache_List *)(pcache->pstrategy)),pbh);
-	
 	return pbh;
 }
 
-
+/* Ne fait rien pour un FIFO*/
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
 {
 }  
 
+/* Ne fait rien pour un FIFO*/
 void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
 {
 } 
 
+/* Renvoie "FIFO" pour décrire la stratégie*/
 char *Strategy_Name()
 {
     return "FIFO";
